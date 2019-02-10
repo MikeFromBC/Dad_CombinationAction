45de5533a492409acf96aa33e7d8a23f1e4bf419
Mike P <lizard@xrw.bc.ca>
2019-02-10 10:42:48 -0800

    add interactivity

--- Stripped/Stripped.ino
+++ Stripped/Stripped.ino
@@ -19,11 +19,20 @@ StopDriver* driver_CH_GT;
 StopDriver* driver_SW_PD;
 MidiReader* midiReader;
 
+enum TestMode {tmNormal, tmSequential, tmLowLevelCycle, tmCycleAll, tmCycleLeft, tmCycleRight};
+
+TestMode eTestMode = tmNormal;
+bool bDebug = false;
+
+String sCommandBuffer;
+
 void setup() {
   debugSerial = &SoftwareSerial(DEBUG_RX_PORT, DEBUG_TX_PORT);
   
   debugSerial->begin(19200);
   debugSerial->println("Starting...");
+
+  sCommandBuffer = "";
 
   pinMode(HEART_BEAT_LED, OUTPUT);
 
@@ -43,82 +52,61 @@ void setup() {
 //  driver_CH_GT = new StopDriver(36, 38, 40, 12);  
 }
 
-void loop() {
-  unsigned long iGreat = 0;
-  unsigned long iChior = 0;
-  unsigned long iSwell = 0;
-  unsigned long iPedal = 0;
 
-//while (1)
-//  midiReader->readMessages();
+void handleCommands() {
+  while (Serial.available() > 0) {
+    // get incoming byte:
+    char c = Serial.read();
 
-/*
-while(1) {
-  driver_CH_GT->testSetAllActive();
-  delay(500);
-  driver_CH_GT->setAllOff();
-  delay(500);
+    // discard unused char
+    if (c == 10)
+      continue;
 
-  driver_CH_GT->test_SetAllInactive();
-  delay(500);
-  driver_CH_GT->setAllOff();
-  delay(500);
-}
-*/
+    if (c == 13) {
+      Serial.println("Executing command:  " + sCommandBuffer);
 
-//debugSerial->print("   DEACT ");
-//unsigned long iDivStops = 3;
-//unsigned long i = driver_CH_GT->calcDeactivation(iDivStops, 0);
-//debugSerial->println(i, HEX);
-//
-//while (1) ;
+      if (sCommandBuffer.equalsIgnoreCase("Normal")) {
+        eTestMode = tmNormal;
+        Serial.println("Normal mode selected");
+      } else if (sCommandBuffer.equalsIgnoreCase("Seq")) {
+        eTestMode = tmSequential;
+        Serial.println("Sequential mode selected");
+      } else if (sCommandBuffer.equalsIgnoreCase("LLCycle")) {
+        eTestMode = tmLowLevelCycle;
+        Serial.println("Low Level Cycle mode selected");
+      } else if (sCommandBuffer.equalsIgnoreCase("CycleAll")) {
+        eTestMode = tmCycleAll;
+        Serial.println("Cycle All mode selected");
+      } else if (sCommandBuffer.equalsIgnoreCase("CycleLeft")) {
+        eTestMode = tmCycleLeft;
+        Serial.println("Cycle Left mode selected");
+      } else if (sCommandBuffer.equalsIgnoreCase("CycleRight")) {
+        eTestMode = tmCycleRight;
+        Serial.println("Cycle Right mode selected");
+      } else if (sCommandBuffer.equalsIgnoreCase("DebugOn")) {
+        bDebug = true;
+        Serial.println("Debug is now on");
+      } else if (sCommandBuffer.equalsIgnoreCase("DebugOff")) {
+        bDebug = false;
+        Serial.println("Debug is now off");
+      } else
+        Serial.println("Unrecognized command!");
 
+      Serial.println();
 
-// TEST:  operate all stops ON, OFF, repeat
-while(1) {
-  debugSerial->print("PART ON:   ");
-  //2048, 1
-  driver_CH_GT->send((unsigned long) 0xffffffff, 0,   // CH
-                     (unsigned long) 0xffffffff, 0);  // GT
-  driver_SW_PD->send((unsigned long) 0xffffffff, 0,   // SW
-                     (unsigned long) 0xffffffff, 0);  // PD
-
-  delay(STOP_DRIVE_TIME_MS);
-
-  driver_CH_GT->setAllOff();
-  driver_SW_PD->setAllOff();
-                       
-  delay(1000);
-
-  debugSerial->print("PART OFF:  ");
-  driver_CH_GT->send(0, 0,   // CH
-                     0, 0);  // GT
-  driver_SW_PD->send(0, 0,   // SW
-                     0, 0);  // PD
-                     
-  delay(STOP_DRIVE_TIME_MS);
-
-  driver_CH_GT->setAllOff();
-  driver_SW_PD->setAllOff();
-
-  delay(1000);
+      sCommandBuffer = "";
+    } else
+      sCommandBuffer += c;
+  }
 }
 
 
+void normalRun() {
+    unsigned long iGreat = 0;
+    unsigned long iChior = 0;
+    unsigned long iPedal = 0;
+    unsigned long iSwell = 0;
 
-// operate one stop at a time on all divisions at once
-//while(1) {
-//  int iStop=1;
-//  for (int i=0; i<20; i++) 
-//  {
-//    driver_CH_GT->send(iStop, 0,   // CH
-//                       iStop, 0);  // GT
-//    iStop=iStop<<1;
-//    delay(1000);
-//  }
-//}
-
-  while (1) {
     midiReader->readMessages();
   
     if (!digitalRead(MIDI_BOARD_SWITCH_PROG))
@@ -165,22 +153,119 @@ while(1) {
       driver_CH_GT->setAllOff();
       driver_SW_PD->setAllOff();
     }
-  } 
+}  // normalRun
 
-//  driver_CH_GT->send(2, 0,   // CH
-//                     2, 0);  // GT
-//                     
-//  delay(1000);
-//  
-//  driver_CH_GT->send(3, 0,   // CH
-//                     3, 0);  // GT
-//
-//  delay(1000);
-//  
-//  driver_CH_GT->send(0, 0,   // CH
-//                     0, 0);  // GT
-//
-//  delay(1000);
 
+void loop() {
+  // TEST:  operate all stops ON, OFF, repeat
+  while(1) {
+    handleCommands();
+  
+    switch (eTestMode) {
+      tmNormal:
+        normalRun();
+        break;
+  
+      tmSequential:
+        int iStop=1;
+        for (int i=0; i<20; i++) 
+        {
+          driver_CH_GT->send(iStop, 0,   // CH
+                             iStop, 0);  // GT
+          iStop=iStop<<1;
+          delay(1000);
+        }
+        break;
+  
+      tmLowLevelCycle:
+        driver_CH_GT->testSetAllActive();
+        delay(STOP_DRIVE_TIME_MS);
+        driver_CH_GT->setAllOff();
+        delay(500);
+      
+        driver_CH_GT->testSetAllInactive();
+        delay(STOP_DRIVE_TIME_MS);
+        driver_CH_GT->setAllOff();
+        delay(500);
+  
+        break;
+        
+      tmCycleAll: 
+        debugSerial->print("PART ON:   ");
+        //2048, 1
+        driver_CH_GT->send((unsigned long) 0xffffffff, 0,   // CH
+                           (unsigned long) 0xffffffff, 0);  // GT
+        driver_SW_PD->send((unsigned long) 0xffffffff, 0,   // SW
+                           (unsigned long) 0xffffffff, 0);  // PD
+      
+        delay(STOP_DRIVE_TIME_MS);
+      
+        driver_CH_GT->setAllOff();
+        driver_SW_PD->setAllOff();
+                             
+        delay(1000);
+      
+        debugSerial->print("PART OFF:  ");
+        driver_CH_GT->send(0, 0,   // CH
+                           0, 0);  // GT
+        driver_SW_PD->send(0, 0,   // SW
+                           0, 0);  // PD
+                           
+        delay(STOP_DRIVE_TIME_MS);
+      
+        driver_CH_GT->setAllOff();
+        driver_SW_PD->setAllOff();
+      
+        delay(1000);
+        break;
+      
+      tmCycleLeft:
+        debugSerial->print("PART ON:   ");
+        //2048, 1
+        driver_CH_GT->send((unsigned long) 0xffffffff, 0,   // CH
+                           (unsigned long) 0xffffffff, 0);  // GT
+      
+        delay(STOP_DRIVE_TIME_MS);
+      
+        driver_CH_GT->setAllOff();
+                             
+        delay(1000);
+      
+        debugSerial->print("PART OFF:  ");
+        driver_CH_GT->send(0, 0,   // CH
+                           0, 0);  // GT
+                           
+        delay(STOP_DRIVE_TIME_MS);
+      
+        driver_CH_GT->setAllOff();
+      
+        delay(1000);
+        break;
+      
+      tmCycleRight:  
+        debugSerial->print("PART ON:   ");
+        //2048, 1
+        driver_SW_PD->send((unsigned long) 0xffffffff, 0,   // SW
+                           (unsigned long) 0xffffffff, 0);  // PD
+      
+        delay(STOP_DRIVE_TIME_MS);
+      
+        driver_SW_PD->setAllOff();
+                             
+        delay(1000);
+      
+        debugSerial->print("PART OFF:  ");
+        driver_SW_PD->send(0, 0,   // SW
+                           0, 0);  // PD
+                           
+        delay(STOP_DRIVE_TIME_MS);
+      
+        driver_SW_PD->setAllOff();
+      
+        delay(1000);
+        break;
+    }
+  }
+  
   flashHeartbeatLED();
 }
