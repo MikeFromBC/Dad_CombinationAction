22aadabf5f17ba4e2b273f01bb08a417a6658c5c
Mike Pijl <sourcetree@xrw.bc.ca>
2019-04-06 09:48:30 -0700

    rework MIDI into using raw serial comm

--- Combo/MidiReader.cpp
+++ Combo/MidiReader.cpp
@@ -1,79 +1,120 @@
 #include <SoftwareSerial.h>
-#include <MIDI.h>
+#include <Arduino.h>
 
 #include "Utils.h"
 #include "Types.h"
 #include "MidiReader.h"
 #include "StopState.h"
 
+#define NOTE_OFF_MSG_NIBBLE         0x80
+#define NOTE_ON_MSG_NIBBLE          0x90
+#define AFTERTOUCH_MSG_NIBBLE       0xA0
+#define CONTROLLER_MSG_NIBBLE       0xB0
+#define PITCH_CHANGE_MSG_NIBBLE     0xC0
+#define CHANNEL_PRESSURE_MSG_NIBBLE 0xD0
+#define PITCH_BEND_MSG_NIBBLE       0xE0
+#define SYSTEM_MSG_NIBBLE           0xF0
+
 #define MIDI_CMD_ACTIVATE    73
 #define MIDI_CMD_DEACTIVATE  74
 
-#define SYNDYNE_PEDAL_CHANNEL 2
-#define SYNDYNE_GREAT_CHANNEL 3
-#define SYNDYNE_SWELL_CHANNEL 4
-#define SYNDYNE_CHIOR_CHANNEL 5 
+#define SYNDYNE_PEDAL_CHANNEL 1
+#define SYNDYNE_GREAT_CHANNEL 2
+#define SYNDYNE_SWELL_CHANNEL 3
+#define SYNDYNE_CHIOR_CHANNEL 4 
 
 extern SoftwareSerial* debugSerial;
-
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
-
 
 MidiReader::MidiReader(StopState* _stopState) {
   stopState = _stopState;
   
-  // We want to receive messages on all channels
-  MIDI.begin(MIDI_CHANNEL_OMNI);
-  
-  // We also want to echo the input to the output, 
-  // so the sniffer can be dropped inline when things misbehave.
-  MIDI.turnThruOn();
+  Serial.begin(31250);
+
+  // clear buffers
+  while (Serial.available()) {
+    Serial.read();
+  }
 }
 
 
 void MidiReader::readMessages() {
-  if (MIDI.read())
-  {
-    switch (MIDI.getType())
-    {
-      case midi::ControlChange:
-        {  /*
-          debugSerial->print("Controller, chan: ");
-          debugSerial->print(MIDI.getChannel());
-          debugSerial->print(" Controller#: ");
-          debugSerial->print(MIDI.getData1());
-          debugSerial->print(" Value: ");
-          debugSerial->println(MIDI.getData2());  */
+  bool bOn;
+  byte iCmd;
+  int iDiv;
+  int iStopNum;
+         
+  while (Serial.available()) {
+    int iBasicCommand = Serial.read();
+    int iBasicCommandNibble = iBasicCommand & 0xF0;
 
-          byte iCmd = MIDI.getData1();
-          bool bOn = iCmd == MIDI_CMD_ACTIVATE;
-    
-          int iDiv = MIDI.getChannel();
-          int iStopNum = MIDI.getData2() - 1;
-   
-          switch (iDiv)
-          {
-            case SYNDYNE_SWELL_CHANNEL:
-                stopState->setSwellStop(iStopNum, bOn);
-                break;
+debugSerial->print("read: ");
+debugSerial->println(iBasicCommand);
 
-            case SYNDYNE_GREAT_CHANNEL:
-                stopState->setGreatStop(iStopNum, bOn);
-                break;
-                  
-            case SYNDYNE_PEDAL_CHANNEL:
-                stopState->setPedalStop(iStopNum, bOn);
-                break;
-                  
-            case SYNDYNE_CHIOR_CHANNEL:
-                stopState->setChiorStop(iStopNum, bOn);
-                break;
-          }  // switch iDiv
+    switch (iBasicCommandNibble) {
 
-          //stopState->debug_ShowAllStopStates("normal");
-        }  // case
-        break;  
-    }  // switch msg type 
-  }  // if
+      case NOTE_OFF_MSG_NIBBLE:
+      case NOTE_ON_MSG_NIBBLE:
+      case AFTERTOUCH_MSG_NIBBLE:
+        // eat
+        Serial.read();
+        Serial.read();
+        break;
+
+      case CONTROLLER_MSG_NIBBLE:
+        iDiv = iBasicCommand & 0x0F;
+
+        // data 1
+        iCmd = Serial.read();
+        bOn = iCmd == MIDI_CMD_ACTIVATE;
+
+        // data 2
+        iStopNum = Serial.read() - 1;
+         
+//        debugSerial->print("Controller, chan: ");
+//        debugSerial->print(iDiv);
+//        debugSerial->print(" Data 1 (cmd): ");
+//        debugSerial->print(iCmd);
+//        debugSerial->print(" Data 2 (value): ");
+//        debugSerial->println(iStopNum);  
+
+        switch (iDiv)
+        {
+          case SYNDYNE_SWELL_CHANNEL:
+              stopState->setSwellStop(iStopNum, bOn);
+              break;
+
+          case SYNDYNE_GREAT_CHANNEL:
+              stopState->setGreatStop(iStopNum, bOn);
+              break;
+                
+          case SYNDYNE_PEDAL_CHANNEL:
+              stopState->setPedalStop(iStopNum, bOn);
+              break;
+                
+          case SYNDYNE_CHIOR_CHANNEL:
+              stopState->setChiorStop(iStopNum, bOn);
+              break;
+        }  // switch iDiv
+
+        //stopState->debug_ShowAllStopStates("normal");
+  
+        break;
+
+      case PITCH_CHANGE_MSG_NIBBLE:
+      case CHANNEL_PRESSURE_MSG_NIBBLE:
+        // no data
+        break;
+
+      case PITCH_BEND_MSG_NIBBLE:
+        // eat
+        Serial.read();
+        Serial.read();
+        break;
+
+      case SYSTEM_MSG_NIBBLE:
+        // ?
+        break;
+    }  // switch msg type
+  }  // while
 }
 
